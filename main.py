d004de3704ab618b6e8611c66fe852f96ae9c48d
VeryCreativeName64 <marekbendeguz@gmail.com>
2024-10-24 11:11:33 +0200

    Mára készek vagyunk

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ jegyek=[5,2,3,4,4,5,5,5,1,2,2,3,4,5,5,5,4,3,3]
 
 
 #1
-#e=metodus.atlag(jegyek)
-#print(f"Az osztályátlag:{e}")
+e=metodus.atlag(jegyek)
+print(f"Az osztályátlag:{e}")
 
 
 #2
-#e=metodus.otosok_szama(jegyek)
-#print(f"Az ötösök száma: {e}")
+e=metodus.otosok_szama(jegyek)
+print(f"Az ötösök száma: {e}")
 
 #3
 e=metodus.egyesek_szama(jegyek)

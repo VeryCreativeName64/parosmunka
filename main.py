40a4ad9102bfd06fdc7d6c54c768300b0ce2180b
VeryCreativeName64 <marekbendeguz@gmail.com>
2024-10-24 10:39:49 +0200

    Jegyek átlaga

---
+++ main.py
@@ -0,0 +1,5 @@
+import metodus
+jegyek=[5,2,3,4,4,5,5,5,1,2,2,3,4,5,5,5,4,3,3,]
+#1
+e=metodus.atlag(jegyek)
+print(f"Az osztályátlag:{e}")
--- metodus.py
+++ metodus.py
@@ -1,1 +1,13 @@
-print("suck")
+
+
+#feladat 1
+def atlag(jegyek):
+    total=0
+    i=0
+    while(i<len(jegyek)):
+        total+=jegyek[i]
+        i+=1
+
+    eredmeny=total/len(jegyek)
+    
+    return eredmeny

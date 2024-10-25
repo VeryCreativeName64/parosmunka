924542875cb9917dccb2a94f422fa66e3a1e26d0
Domawas <dominknagy@gmail.com>
2024-10-25 11:51:18 +0200

    Kész A Feladat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import metodus
 jegyek=[5,2,3,4,4,5,5,5,1,2,2,3,4,5,5,5,4,3,3]
-
+diak_nevek = ["Anna", "Béla", "Cecil", "Dávid", "Erika", "Ferenc", "Gábor", "Hanna", "Ildikó", "János", "Katalin", "László", "Mária", "Nóra", "Olivér", "Péter", "Rita", "Sándor", "Tímea"]
 
 #1
 e=metodus.atlag(jegyek)
@@ -15,6 +15,16 @@ print(f"Az ötösök száma: {e}")
 e=metodus.egyesek_szama(jegyek)
 print(f"{e} darab diák bukott meg.")
 
+#4
+diak_szama=metodus.elegtelen(jegyek)
+print(f"Az elégtelent kapott diákok száma: {diak_szama}")
 
+#5
+metodus.sávdiagram(jegyek)
 
+#6
+metodus.diak_jegyek(jegyek, diak_nevek)
 
+#7
+diakszamok=metodus.diak_jegyek_generalasa(17,tobb_5_os=False)
+print(f"Diákok véletlen jegyei:{diakszamok}")
--- metodus.py
+++ metodus.py
@@ -19,7 +19,7 @@ def otosok_szama(jegyek):
             otos+=1
         i+=1
     return otos
-
+#3
 def egyesek_szama(jegyek):
     i=0
     egyes=0
@@ -29,4 +29,53 @@ def egyesek_szama(jegyek):
         i+=1
     return egyes
 
+#4
+def elegtelen(jegyek):
+    diak_szama=0
+    i=0
+    while (i<len(jegyek)):
+        if jegyek[i]==1:
+            diak_szama+=1
+        i+=1
+    return diak_szama
 
+#5
+def sávdiagram(jegyek):
+    szamok = [0] * 5 
+    i = 0
+
+    while i < len(jegyek):
+        jegy = jegyek[i]
+        if 1 <= jegy <= 5:
+            szamok[jegy - 1] += 1
+        i += 1
+
+    print("Sávdiagram:")
+    i = 0
+    while i < 5:
+        print(f"{i + 1}| {'*' * szamok[i]}")
+        i += 1
+
+#6
+def diak_jegyek(jegyek, diak_nevek):
+    """Megjeleníti a diákok jegyeit látványos formában."""
+    i = 0
+    print("Diákok jegyei:")
+    while i < len(jegyek):
+        print(f"{i + 1}. diák: {diak_nevek[i]}: {jegyek[i]}")
+        i += 1
+
+import random
+def diak_jegyek_generalasa(szam, tobb_5_os=False):
+    """Véletlen diák jegyek generálása."""
+    diakszamok = []
+    i = 0
+
+    while i < szam:
+        if tobb_5_os and random.random() < 0.67:
+            diakszamok.append(5)
+        else:
+            diakszamok.append(random.randint(1, 4))
+        i += 1
+
+    return diakszamok
